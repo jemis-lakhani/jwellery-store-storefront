a66e6b8eabea75f722e027e7e8a2dca8e6d01118
jemis-lakhani <jemislakhani.tech@gmail.com>
2024-04-04 12:50:11 +0530

    - Font setup added. - UI theme setup added.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,14 @@
 import { Metadata } from "next"
 import "styles/globals.css"
+import { Inter } from "next/font/google"
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://localhost:8000"
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+})
 
 export const metadata: Metadata = {
   metadataBase: new URL(BASE_URL),
@@ -9,7 +16,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
-    <html lang="en" data-mode="light">
+    <html lang="en" data-mode="light" className={inter.className}>
       <body>
         <main className="relative">{props.children}</main>
       </body>
