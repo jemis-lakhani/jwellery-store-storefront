c1b09b344719eb46ffa594f1f22050587dfdc4d5
jemis-lakhani <jemislakhani.tech@gmail.com>
2024-04-05 15:54:43 +0530

    icon and font added

--- src/modules/layout/templates/footer/index.tsx
+++ src/modules/layout/templates/footer/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import instagram from '../../../../../public/instagram.svg'
+import facebook from '../../../../../public/facebook.svg'
+import Image from 'next/image';
 
 const Footer = () => {
   return (
     <footer>
       <hr />
-      <div className='flex justify-around bg-red-100'>
+      <div className='flex justify-around bg-red-100 text-base font-serif'>
         <div>
-        <h2 className='mt-5'><b className='-ml-3'>Contact Us</b></h2>
-        <ul className='pt-3 list-disc'>
-          <a href="\"><li>Customer Care</li></a>
-          <a href="\"><li>WhatsApp</li></a>
-          <a href="\"><li>Email</li></a>
-        </ul>
+          <h2 className='mt-5'><b className='-ml-3'>Contact Us</b></h2>
+          <ul className='pt-3 list-disc'>
+            <a href="\"><li>Customer Care</li></a>
+            <a href="\"><li>WhatsApp</li></a>
+            <a href="\"><li>Email</li></a>
+          </ul>
         </div>
         <div>
           <h2 className='mt-5'><b className='-ml-3'>Quick Links</b></h2>
@@ -21,20 +24,30 @@ const Footer = () => {
             <a href="\"><li>About Us</li></a>
             <a href="\"><li>Customer Reviews</li></a>
             <a href="\"><li>FAQ</li></a>
-        </ul>
+          </ul>
         </div>
         <div>
           <h2 className='mt-5'><b className='-ml-3'>Follow Us</b></h2>
-          <ul className='pt-3 list-disc'>
-            <li>Instagram</li>
-            <li>Facebook</li>
+          <ul className='pt-3'>
+            <li>
+              <a className='flex' href="\">
+                <Image src={instagram} alt="img" height={32}></Image>
+                <span className='ml-1 text-lg'>Instagram</span>
+              </a>
+            </li>
+            <li>
+              <a className='flex' href="\">
+                <Image src={facebook} alt="img" height={32}></Image>
+                <span className='text-lg ml-1'>Facebook</span>
+              </a>
+            </li>
           </ul>
         </div>
       </div>
-      <div className="txt-compact-small text-black h-8 bg-red-200 text-center flex justify-center item-center">
-          <span>
-            © {new Date().getFullYear()} Evara Jwellary Store. All rights reserved
-          </span>
+      <div className="txt-compact-small text-black h-8 bg-red-200 text-center flex justify-center items-center">
+        <span>
+          © {new Date().getFullYear()} Evara Jwellary Store. All rights reserved
+        </span>
       </div>
     </footer>
   )
