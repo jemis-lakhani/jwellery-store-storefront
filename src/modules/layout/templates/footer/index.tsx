2627e90aa1e085e8bca583fd4b408089bb5410bb
jemis-lakhani <146762062+jemis-lakhani@users.noreply.github.com>
2024-04-06 10:51:00 +0530

    Footer In Progress (#2)
    * started with footer
    
    * icon and font added
    
    * updated

--- src/modules/layout/templates/footer/index.tsx
+++ src/modules/layout/templates/footer/index.tsx
@@ -1,152 +1,56 @@
-import { Text, clx } from "@medusajs/ui"
+import React from 'react';
+import instagram from '../../../../../public/instagram.svg'
+import facebook from '../../../../../public/facebook.svg'
+import Image from 'next/image';
 
-import { getCategoriesList, getCollectionsList } from "@lib/data"
-
-import LocalizedClientLink from "@modules/common/components/localized-client-link"
-import MedusaCTA from "@modules/layout/components/medusa-cta"
-
-export default async function Footer() {
-  const { collections } = await getCollectionsList(0, 6)
-  const { product_categories } = await getCategoriesList(0, 6)
-
+const Footer = () => {
   return (
-    <footer className="border-t border-ui-border-base w-full">
-      <div className="content-container flex flex-col w-full">
-        <div className="flex flex-col gap-y-6 xsmall:flex-row items-start justify-between py-40">
-          <div>
-            <LocalizedClientLink
-              href="/"
-              className="txt-compact-xlarge-plus text-ui-fg-subtle hover:text-ui-fg-base uppercase"
-            >
-              Medusa Store
-            </LocalizedClientLink>
-          </div>
-          <div className="text-small-regular gap-10 md:gap-x-16 grid grid-cols-2 sm:grid-cols-3">
-            {product_categories && product_categories?.length > 0 && (
-              <div className="flex flex-col gap-y-2">
-                <span className="txt-small-plus txt-ui-fg-base">
-                  Categories
-                </span>
-                <ul className="grid grid-cols-1 gap-2" data-testid="footer-categories">
-                  {product_categories?.slice(0, 6).map((c) => {
-                    if (c.parent_category) {
-                      return
-                    }
-
-                    const children =
-                      c.category_children?.map((child) => ({
-                        name: child.name,
-                        handle: child.handle,
-                        id: child.id,
-                      })) || null
-
-                    return (
-                      <li
-                        className="flex flex-col gap-2 text-ui-fg-subtle txt-small"
-                        key={c.id}
-                      >
-                        <LocalizedClientLink
-                          className={clx(
-                            "hover:text-ui-fg-base",
-                            children && "txt-small-plus"
-                          )}
-                          href={`/categories/${c.handle}`}
-                          data-testid="category-link"
-                        >
-                          {c.name}
-                        </LocalizedClientLink>
-                        {children && (
-                          <ul className="grid grid-cols-1 ml-3 gap-2">
-                            {children &&
-                              children.map((child) => (
-                                <li key={child.id}>
-                                  <LocalizedClientLink
-                                    className="hover:text-ui-fg-base"
-                                    href={`/categories/${child.handle}`}
-                                    data-testid="category-link"
-                                  >
-                                    {child.name}
-                                  </LocalizedClientLink>
-                                </li>
-                              ))}
-                          </ul>
-                        )}
-                      </li>
-                    )
-                  })}
-                </ul>
-              </div>
-            )}
-            {collections && collections.length > 0 && (
-              <div className="flex flex-col gap-y-2">
-                <span className="txt-small-plus txt-ui-fg-base">
-                  Collections
-                </span>
-                <ul
-                  className={clx(
-                    "grid grid-cols-1 gap-2 text-ui-fg-subtle txt-small",
-                    {
-                      "grid-cols-2": (collections?.length || 0) > 3,
-                    }
-                  )}
-                >
-                  {collections?.slice(0, 6).map((c) => (
-                    <li key={c.id}>
-                      <LocalizedClientLink
-                        className="hover:text-ui-fg-base"
-                        href={`/collections/${c.handle}`}
-                      >
-                        {c.title}
-                      </LocalizedClientLink>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            )}
-            <div className="flex flex-col gap-y-2">
-              <span className="txt-small-plus txt-ui-fg-base">Medusa</span>
-              <ul className="grid grid-cols-1 gap-y-2 text-ui-fg-subtle txt-small">
-                <li>
-                  <a
-                    href="https://github.com/medusajs"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="hover:text-ui-fg-base"
-                  >
-                    GitHub
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://docs.medusajs.com"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="hover:text-ui-fg-base"
-                  >
-                    Documentation
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://github.com/medusajs/nextjs-starter-medusa"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="hover:text-ui-fg-base"
-                  >
-                    Source code
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
+    <footer>
+      <hr />
+      <div className='flex justify-around bg-red-100 text-base font-serif'>
+        <div>
+          <h2 className='mt-5'><b className='-ml-3'>Contact Us</b></h2>
+          <ul className='pt-3 list-disc'>
+            <a href="\"><li>Customer Care</li></a>
+            <a href="\"><li>WhatsApp</li></a>
+            <a href="\"><li>Email</li></a>
+          </ul>
         </div>
-        <div className="flex w-full mb-16 justify-between text-ui-fg-muted">
-          <Text className="txt-compact-small">
-            © {new Date().getFullYear()} Medusa Store. All rights reserved.
-          </Text>
-          <MedusaCTA />
+        <div>
+          <h2 className='mt-5'><b className='-ml-3'>Quick Links</b></h2>
+          <ul className='pt-3 list-disc pb-8'>
+            <a href="\"><li>Home</li></a>
+            <a href="\"><li>Category</li></a>
+            <a href="\"><li>About Us</li></a>
+            <a href="\"><li>Customer Reviews</li></a>
+            <a href="\"><li>FAQ</li></a>
+          </ul>
         </div>
+        <div>
+          <h2 className='mt-5'><b className='ml-2'>Follow Us</b></h2>
+          <ul className='pt-3'>
+            <li>
+              <a className='flex' href="\">
+                <Image src={instagram} alt="img" height={32}></Image>
+                <span className='ml-1 text-lg'>Instagram</span>
+              </a>
+            </li>
+            <li className='mt-1'>
+              <a className='flex' href="\">
+                <Image src={facebook} alt="img" height={32}></Image>
+                <span className='text-lg ml-1'>Facebook</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="txt-compact-small text-black h-8 bg-red-200 text-center flex justify-center items-center">
+        <span>
+          © {new Date().getFullYear()} Evara Jwellary Store. All rights reserved
+        </span>
       </div>
     </footer>
   )
 }
+
+export default Footer;
